1753c1ecc16b2717f603bf823b4074e6593b0aa7
Dogfoger <39027628+Dogfoger@users.noreply.github.com>
2020-02-18 15:43:48 +0200

    Added comment
    Added commit

--- test/src/main/java/org/example/App.java
+++ test/src/main/java/org/example/App.java
@@ -5,6 +5,7 @@ public class App {
 
     public static void main(String[] args) {
         Life l = new Life();
+        //Edit for master
         //System.out.println(l.randState(9,8));
         l.renderBoard(l.randState(50,100));
     }
